52661aea4f45fadcba25b734442de43104fc031d
Jesse Zhu <jesse@itjesse.cn>
2015-03-15 05:09:48 +0800

    更新菜单设置

--- js/button.js
+++ js/button.js
@@ -2,6 +2,22 @@ $(document).on('ready', function() {
     var mark = window.location.href.split('#');
     $('[id='+mark[1]+']').parent().addClass('active');
 
+    var title = '';
+    switch(mark[1]){
+        case 'record':
+            title = '图书录入';
+            break;
+        case 'manage':
+            title = '图书管理';
+            break;
+        case 'setting':
+            title = '串口设置';
+            break;
+        default:
+            title = '智慧图书馆';
+    }
+    $('#title').text(title);
+
     $("#record").on('click', function() {
         chrome.app.window.create('record.html#record', {
             'bounds': {
